fa5472d3f64f8baba97eb7a16f5ddee3a893aabc
ShikariBaaZ <91068760+ShikariBaaZ@users.noreply.github.com>
2022-04-06 21:22:57 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/ShikariBaaZ/Telegraph_Img_Uploader.git /Telegraph_Img_Uploader
+cd /Telegraph_Img_Uploader
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
